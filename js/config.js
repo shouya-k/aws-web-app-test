871352c2bb7bee67aa7b13c60300e9b8ea8c38a7
光菅翔也 <idb8bzputt@gmail.com>
2021-05-16 22:48:48 +0900

    fix: config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: 'us-east-1_bJqlaLz3t', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '72no9u3vgk8r4p9bkt7jb3tb5t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'us-east-1', // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
